c6aee9a801655f4f7817c8b80107322c5bb3be4d
bbbb <bgh9651@gmail.com>
2022-06-22 22:09:04 +0900

    ADD 장바구니 기능 구현 중 레이아웃 구현

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Main from '../src/pages/Main/Main';
-import Nav from '../src/components/Nav/Nav';
+
 import ProductDetail from './components/ProductDetail/ProductDetail';
 import Footer from '../src/components/Footer/Footer';
 import Login from '../src/components/Login/Login';
+import Cart from './components/Cart/Cart';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Cart />
       <Routes>
         <Route path="/main" element={<Main />} />
         <Route path="/login" element={<Login />} />
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Cart.scss';
 
 const Cart = () => {
-  return <div></div>;
+  const [CartList, setCartList] = useState([]);
+
+  fetch('/data/itemsData.json')
+    .then(res => res.json())
+    .then(data => setCartList(data));
+
+  return (
+    <div className="main">
+      <div className="overlay" />
+      <div className="cart">
+        <div className="title">
+          <div className="title_span_box">
+            <span className="title_header"> YOUR CART</span>
+            <span className="title_text">is empty! </span>
+          </div>
+          <button className="close_btn">X</button>
+        </div>
+        {/* <CartBtn /> */}
+        <CartItems CartList={CartList} />
+        <div className="total_price_box">
+          <span className="total_text">Estimated total</span>
+          <span className="total_price"> 22 </span>
+        </div>
+        <button className="checkout_btn">CHECKOUT</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
+
+const CartBtn = () => {
+  return (
+    <div className="btn_box">
+      <button className="cart_firstbtn">
+        SHOP DROP + THE LORD OF THE RINGS
+      </button>
+      <button className="cart_btn"> SHOP DROP + MARVEL</button>
+      <button className="cart_btn"> SHOP DROP ICON COLLECTION KEYBOARDS</button>
+    </div>
+  );
+};
+
+const CartItems = ({ CartList }) => {
+  return (
+    <>
+      {CartList.map(el => {
+        return (
+          <div className="items">
+            <img src={el.img} alt="item" className="items_img" />
+            <div className="items_description">
+              <span className="items_name">{el.products}</span>
+              <span className="items_option">
+                {el.color}
+                {` / `}
+                {el.option}
+              </span>
+            </div>
+            <div className="items_delete_price">
+              <button className="items_remove">Remove</button>
+              <span className="items_price">{el.price}</span>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
