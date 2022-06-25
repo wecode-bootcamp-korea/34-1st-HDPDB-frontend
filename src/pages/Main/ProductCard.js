ef977587115f47af2227f4f0c7a5c53be0b05925
kyungbaa <cocoroto@naver.com>
2022-06-25 17:22:55 +0900

    ADD : main aside nav 레이아웃

--- src/pages/Main/ProductCard.js
+++ src/pages/Main/ProductCard.js
@@ -3,7 +3,18 @@ import './ProductCard.scss';
 const ProductCard = () => {
   return (
     <div className="productCard">
-      <div>ddd</div>
+      <div className="card_header">
+        <div className="card_icon_box">
+          <img
+            className="card_icon"
+            src="./images/logo/drop-favicon.svg"
+            alt="product list drop logo icon"
+          />
+        </div>
+      </div>
+      <div className="card_contents">
+        <img src="image/productList/"></img>
+      </div>
     </div>
   );
 };
