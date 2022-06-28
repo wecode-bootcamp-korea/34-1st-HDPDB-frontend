c5cc1db9ad614f2efb6a1b8a52d3977aaa296a58
kyungbaa <cocoroto@naver.com>
2022-06-28 18:28:16 +0900

    ADD: variablers 항목 추가 및 index.js import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client';
 import Router from './Router';
 import '../src/styles/reset.scss';
 import '../src/styles/common.scss';
-
+import '../src/styles/variables.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Router />);
